69ef365fa18dd348e05f74d6486aa95f296f4586
Sergey Kucherov <skch@usa.net>
2018-08-15 14:03:44 -0700

    Clean the using statements

--- dot-net-core/rest-api/Models/ConfigurationSettings.cs
+++ dot-net-core/rest-api/Models/ConfigurationSettings.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestApiShowcase.Models
 {
 	public class ConfigurationSettings
 	{
-		public ConfigAppSettings AppSettings { get; set; }
-	
+		public ConfigAppSettings AppSettings { get; set; }	
 	}
-
 
 	public class ConfigAppSettings
 	{
